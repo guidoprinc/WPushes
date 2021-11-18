858f79c33bff44a38b0b9c2d1364caeb7080642b
guidoprinc <guido.princ@wolox.com.ar>
2021-11-18 16:16:31 -0300

    added remote notificaiton

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -21,6 +21,31 @@ const App = () => {
   }, [dispatch]);
 
   useEffect(() => {
+    // Assume a message-notification contains a "type" property in the data payload of the screen to open
+
+    messaging().onNotificationOpenedApp(remoteMessage => {
+      console.log(
+        'Notification caused app to open from background state:',
+        remoteMessage.notification,
+      );
+      // TODO add functionality
+    });
+
+    // Check whether an initial notification is available
+    messaging()
+      .getInitialNotification()
+      .then(remoteMessage => {
+        if (remoteMessage) {
+          console.log(
+            'Notification caused app to open from quit state:',
+            remoteMessage.notification,
+          );
+          // TODO add other functionality
+        }
+      });
+  }, []);
+
+  useEffect(() => {
     const unsubscribe = messaging().onMessage(async remoteMessage => {
       Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
     });
