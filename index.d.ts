49b4316bb1660302bea7a6fbdf91be2ecf42cb97
guidoprinc <guido.princ@wolox.com.ar>
2021-11-23 17:19:33 -0300

    more modifications

---
+++ index.d.ts
@@ -0,0 +1,8 @@
+declare module '*.jpg';
+declare module '*.json';
+declare module '*.png';
+declare module 'cerealizr';
+declare module 'reactotron-apisauce';
+declare module 'reactotron-react-native/dist/flipper';
+declare module 'redux-persist-seamless-immutable';
+declare module 'redux-recompose';
